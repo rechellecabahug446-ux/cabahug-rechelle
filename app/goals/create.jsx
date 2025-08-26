f5295944834a9e763699d0abad9d4996890e6928
znerrenz <znerruiz@gmail.com>
2025-08-26 08:27:13 +0800

    index and create were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#1a98ecff',
     color: 'white',
     borderRadius: 8,
   }
